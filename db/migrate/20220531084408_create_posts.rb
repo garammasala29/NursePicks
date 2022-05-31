03bfbefa7f99ada1d810b100235daa4e54ac5e57
garammasala29 <kmasa1569@gmail.com>
2022-05-31 17:49:43 +0900

    bin/rails db:migrate

--- db/migrate/20220531084408_create_posts.rb
+++ db/migrate/20220531084408_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.string :url
+      t.string :title, null: false
+      t.string :url, null: false
       t.string :site_name
       t.string :image_url
       t.references :user, null: false, foreign_key: true
