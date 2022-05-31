da3e31543ab7b2cc520232b4c94df86b9b12e4e9
garammasala29 <kmasa1569@gmail.com>
2022-06-01 00:39:06 +0900

    ルーティングの修正と不要なspecファイルを作らないよう修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'posts#index'
+  resources :posts, except: %i(edit update)
 end
