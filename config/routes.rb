69cc58c71dbf3eb9389b797ac17f4428a8a70c12
garammasala29 <kmasa1569@gmail.com>
2022-05-31 22:46:40 +0900

    bin/rails g controller posts index new show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
