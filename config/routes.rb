eecc598f342bc685cc334e3d32aae8aad828b42c
garammasala29 <kmasa1569@gmail.com>
2022-06-03 15:57:32 +0900

    ユーザーの作成、編集、詳細画面を作成した

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/show'
-  get 'users/edit'
   root 'posts#index'
   resources :posts, except: %i(edit update)
+  resources :users, except: :index
 end
