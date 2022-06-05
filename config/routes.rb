5a6706630037dc30695fc65788d862c0e351dcac
garammasala29 <kmasa1569@gmail.com>
2022-06-06 00:48:28 +0900

    サインインページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root 'posts#index'
   resources :posts, except: %i[edit update]
   resources :users, except: :index
+  get '/signup', to: 'users#new'
+  get '/signin', to: 'sessions#new'
   delete '/signout', to: 'sessions#destroy'
   get '/auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
