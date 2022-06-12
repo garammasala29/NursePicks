67173d119268a670cd3bddae9475b272ddda4a85
garammasala29 <kmasa1569@gmail.com>
2022-06-12 18:54:29 +0900

    アソシエーションを追加、DBに反映

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   validates :url, presence: true, uniqueness: true
   validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   def self.from_omniauth(auth_hash)
     provider = auth_hash[:provider]
