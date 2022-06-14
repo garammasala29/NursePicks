1201c5b1999f03ea3d6e17e089f1fe4d2a36b23f
garammasala29 <kmasa1569@gmail.com>
2022-06-14 20:49:46 +0900

    Twitter認証からログイン、アカウント作成できるようにした

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,13 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
     provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET']
+    provider :twitter, ENV['TWITTER_ID'], ENV['TWITTER_SECRET']
   else
     provider :google_oauth2,
       Rails.application.credentials.google[:client_id],
       Rails.application.credentials.google[:client_secret]
+    provider :twitter,
+      Rails.application.credentials.twitter[:client_id],
+      Rails.application.credentials.twitter[:client_secret]
   end
 end
