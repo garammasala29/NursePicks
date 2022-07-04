366e425b00c5102de0587e3723d37121213fea17
garammasala29 <kmasa1569@gmail.com>
2022-07-04 23:02:08 +0900

    フィルタの修正とエラーメッセージを一時非表示にした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :signin_required, only: %i[new create edit update destroy]
+  before_action :signin_required, only: %i[new edit update destroy]
   before_action :set_user, only: %i[show update destroy]
 
   def new; end
