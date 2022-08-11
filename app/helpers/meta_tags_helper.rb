47192b64c529d1a2746e77258567c5555131ff5b
garammasala29 <kmasa1569@gmail.com>
2022-08-11 20:07:28 +0900

    画像を追加し、説明文言を修正

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,16 +1,18 @@
-module ApplicationHelper
-  def default_meta_tags
+# frozen_string_literal: true
+
+module MetaTagsHelper
+  def default_meta_tags # rubocop:disable Metrics/MethodLength
     {
       site: 'NursePicks',
       title: 'NursePicks',
       reverse: true,
       separator: '|',
       charset: 'utf-8',
-      description: '看護師さん向けの情報サイト',
-      keywords: 'キーワード',
+      description: '看護師さんのための情報収集サイト。看護に関する価値ある記事を探したり、おすすめの記事を投稿しよう',
+      keywords: '看護師,情報収集,看護記事,看護研究',
       viewport: 'width=device-width, initial-scale=1.0',
       icon: [
-        { href: image_url('favicon.png') },
+        { href: image_url('favicon.png') }
       ],
       og: {
         site_name: :site,
@@ -19,14 +21,14 @@ module ApplicationHelper
         type: 'website',
         url: 'https://nursepicks.herokuapp.com/',
         image: image_url('ogp.png'),
-        locale: 'ja_JP',
+        locale: 'ja_JP'
       },
       twitter: {
         title: :title,
         card: 'summary_large_image',
         site: '@garammasala29',
         description: :description,
-        image: image_url('ogp.png')
+        image: image_url('ogp.png'),
         domain: 'https://nursepicks.herokuapp.com/'
       }
     }
