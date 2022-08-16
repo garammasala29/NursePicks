3e767c092a191910fedf593473de5bd3e5a7d159
garammasala29 <kmasa1569@gmail.com>
2022-08-16 23:41:52 +0900

    sign_in_asメソッドを使ってログインできることを確認した

--- spec/sign_in_helper.rb
+++ spec/sign_in_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module SignInHelper
+  def sign_in_as(user)
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:twitter]
+    visit root_path
+    click_on 'ログイン'
+    click_on 'Twitterでログイン'
+    @current_user = user
+  end
+
+  def current_user
+    @current_user
+  end
 end
