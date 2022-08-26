75d595da25e5423e1450ec28b720cec01a670c4d
garammasala29 <kmasa1569@gmail.com>
2022-08-26 20:45:49 +0900

    userのシステムスペックを修正
    いくつかalt属性も追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,12 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
-  let(:user) { FactoryBot.create(:user) }
+  let(:user) { create(:user) }
+
+  describe 'show' do
+    it 'ユーザーページが存在すること' do
+      sign_in_as(user)
+      click_on 'nav_user_icon'
+      expect(page).to have_content 'Alice'
+    end
+  end
 
   describe 'create' do
     context 'Twitter認証を使う場合' do
       it 'ユーザーを作成できること' do
-        mock_auth
+        mock_auth(:twitter)
         visit root_path
         expect do
           click_on 'ユーザー登録'
@@ -20,7 +28,7 @@ RSpec.describe 'Users', type: :system do
       end
 
       it 'デフォルト画像を使用してユーザー登録すること' do
-        mock_auth
+        mock_auth(:twitter)
         visit root_path
         expect do
           click_on 'ユーザー登録'
@@ -28,14 +36,14 @@ RSpec.describe 'Users', type: :system do
           choose 'user_icon_url_icon_user_m_png'
           click_on 'アカウント登録'
           expect(page).to have_content 'アカウントを登録しました'
-          # expect(page).to have_selector "img[src$='icon_user_m']" #alt属性を追加してから
+          expect(page).to have_selector "img[src*='icon_user_m']"
         end.to change { User.count }.by(1)
       end
     end
 
     context 'Google認証を使う場合' do
       it 'ユーザーを作成できること' do
-        mock_auth(:google_oauth2)
+        mock_auth(:google)
         visit root_path
         expect do
           click_on 'ユーザー登録'
@@ -52,12 +60,12 @@ RSpec.describe 'Users', type: :system do
     it 'ユーザーの情報を編集できること' do
       sign_in_as(user)
       visit user_path(user)
-      expect do
-        clcik_on 'アカウント編集'
-        fill_in 'アカウント名', with: 'Carol'
-        click_on 'アカウント更新'
-        expect(page).to have_content 'ユーザー情報を編集しました'
-      end
+      click_on 'アカウント編集'
+      fill_in 'アカウント名', with: 'Carol'
+      click_on 'アカウント更新'
+      expect(page).to have_content 'ユーザー情報を編集しました'
+      expect(page).to have_content 'Carol'
+      expect(page).to_not have_content 'Alice'
     end
   end
 
