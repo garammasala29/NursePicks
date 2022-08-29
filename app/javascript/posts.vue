ad4d158c31e544e4e9dcb2ffcdfa40614aa2dd97
garammasala29 <kmasa1569@gmail.com>
2022-08-29 11:20:09 +0900

    画像にalt属性を追加した

--- app/javascript/posts.vue
+++ app/javascript/posts.vue
@@ -15,7 +15,7 @@ section.section.container
           .rank-number
             | {{ index + 1 }}
       .media-left
-        img.image.is-64x64(:src='post.image_url')
+        img.image.is-64x64(:src='post.image_url', alt='post_image')
       .media-content
         .title.is-5
           a(:href='post.url') {{ post.title }}
