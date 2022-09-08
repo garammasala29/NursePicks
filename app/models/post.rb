d50fc2310542fb08bd80ac78be2a20f21fda1adb
garammasala29 <kmasa1569@gmail.com>
2022-09-08 12:08:13 +0900

    記事が投稿された時にツイートされるようにした

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,20 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   validates :url, presence: true, uniqueness: true
   validates :title, presence: true
+
+  def twitter_bot
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+    end
+
+    client.update(<<~TEXT
+      新たに「#{self.title}」が投稿されました！
+      URL：https://www.nursepicks.jp/posts/#{(self.id)}
+      #NursePicks
+      TEXT
+    )
+  end
 end
