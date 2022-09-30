edc1181fcbae39c10a90e3893b32bd5037bbc276
garammasala29 <kmasa1569@gmail.com>
2022-09-30 20:01:43 +0900

    新規登録画面のデザインを修正
    bin/lintによる修正も追加

--- app/javascript/post-tags.js
+++ app/javascript/post-tags.js
@@ -1,17 +1,14 @@
 import Choices from 'choices.js'
 
-
 document.addEventListener('DOMContentLoaded', () => {
-  const elementMultipleSelect = document.getElementById(
-    'js-post-tags'
-  )
-  if (elementMultipleSelect) {
-    return new Choices(elementMultipleSelect, {
+  const element = document.getElementById('js-post-tags')
+  if (element) {
+    return new Choices(element, {
       allowHTML: true,
       delimiter: ',',
       removeItemButton: true,
       addItemText: (value) => {
-        return `Enterを押すと<b>「${value}」</b>のタグが追加されます`;
+        return `Enterを押すと<b>「${value}」</b>のタグが追加されます`
       }
     })
   }
