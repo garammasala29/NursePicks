502cd82a31d3b30d10e574c15413610171fcd5f9
garammasala29 <kmasa1569@gmail.com>
2022-10-10 01:29:51 +0900

    使い方が表示されない場合に「このサイトの使い方」を表示させるようにした

--- app/javascript/hide-usage.js
+++ app/javascript/hide-usage.js
@@ -1,20 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
   const elements = document.querySelectorAll('#js-hide-usage')
+  const usage = document.querySelector('.usage')
+  const message = document.querySelector('.hide-usage-message')
   for (const element of elements) {
-    element.addEventListener('click', () => {
-      const usage = document.querySelector('.usage')
-      if (usage) {
+    if (usage.style.display === 'block') {
+      element.addEventListener('click', () => {
         usage.style.display = 'none'
-      } 
-      // if (document.cookie === 'nursepicks_usage=hide') {
-      //   document.cookie = "nursepicks_usage=''"
-      //   const usage = element.getAttribute('data-usage')
-      //   document.getElementById(usage).classList.toggle('is-active')
-      // } else {
-      //   document.cookie = 'nursepicks_usage=hide'
-      //   const usage = element.getAttribute('data-usage')
-      //   document.getElementById(usage).classList.toggle('is-active')
-      // }
-    })
+        message.style.display = 'block'
+      })
+    } else {
+      element.addEventListener('click', () => {
+        usage.style.display = 'block'
+        message.style.display = 'none'
+      })
+    }
   }
 })
+
+// if (document.cookie === 'nursepicks_usage=hide') {
+//   document.cookie = "nursepicks_usage=''"
+//   const usage = element.getAttribute('data-usage')
+//   document.getElementById(usage).classList.toggle('is-active')
+// } else {
+//   document.cookie = 'nursepicks_usage=hide'
+//   const usage = element.getAttribute('data-usage')
+//   document.getElementById(usage).classList.toggle('is-active')
+// }
